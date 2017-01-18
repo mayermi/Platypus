2f5240bdbb0475c79be6a8ff4a5fb617d99b6ee7
Miriam Mayer <miri.mayer@t-online.de>
2017-01-18 15:42:38 +1000

    Much http

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -3,8 +3,6 @@ import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
 import { HeroService } from './hero.service';
 import 'rxjs/add/operator/switchMap';
-import { Hero } from './hero';
-
 
 @Component({
   moduleId: module.id,
@@ -13,7 +11,6 @@ import { Hero } from './hero';
   styleUrls: [ 'hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-  @Input hero: Hero;
   constructor(
     private heroService: HeroService,
     private route: ActivatedRoute,
@@ -30,6 +27,10 @@ export class HeroDetailComponent implements OnInit {
   this.route.params
     .switchMap((params: Params) => this.heroService.getHero(+params['id']))
     .subscribe(hero => this.hero = hero);
-}
+  }
+
+  save(): void {
+    this.heroService.update(this.hero).then(() => this.goBack());
+  }
 
 }
