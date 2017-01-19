eedac212a75746571bdfaebf4081ea29d1a197bc
Miriam Mayer <miri.mayer@t-online.de>
2017-01-19 15:57:49 +1000

    search works

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,23 +1,18 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppRoutingModule }     from './app-routing.module';
-
+import './rxjs-extensions';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
-
-import { AppComponent }        from './app.component';
-import { DashboardComponent }  from './dashboard.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroService }         from './hero.service';
-import { HeroSearchComponent } from './hero-search.component';
-
-import './rxjs-extensions';
-
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
+import { HeroSearchComponent }  from './hero-search.component';
 @NgModule({
   imports: [
     BrowserModule,
@@ -36,5 +31,4 @@ import './rxjs-extensions';
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule { }
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
-import { Location }                 from '@angular/common';
-import { Hero } from './hero';
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit }      from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
+import { Hero }        from './hero';
 import { HeroService } from './hero.service';
-
 @Component({
   moduleId: module.id,
   selector: 'my-hero-detail',
@@ -17,21 +17,17 @@ export class HeroDetailComponent implements OnInit {
     private route: ActivatedRoute,
     private location: Location
   ) {}
-
-
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
   // !!! Going back too far could take us out of the application. That's acceptable in a demo. We'd guard against it in a real application, perhaps with the CanDeactivate guard.
   goBack(): void {
     this.location.back();
   }
-
-  ngOnInit(): void {
-  this.route.params
-    .switchMap((params: Params) => this.heroService.getHero(+params['id']))
-    .subscribe(hero => this.hero = hero);
-  }
-
-  save(): void {
-    this.heroService.update(this.hero).then(() => this.goBack());
-  }
-
 }
