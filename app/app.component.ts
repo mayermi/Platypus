cc1785d48906bb445863d600c08412adc3d0e50c
Miriam Mayer <miri.mayer@t-online.de>
2017-02-01 19:20:59 +1000

    First attempt login/logout button

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { User } from './_models/index';
+import { UserService } from './_services/index';
 
 @Component({
     moduleId: module.id,
@@ -6,4 +8,22 @@
     templateUrl: 'app.component.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+  loggedin : boolean;
+  loginbutton: string;
+  currentUser: User;
+
+  constructor(private userService: UserService) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  ngDoCheck() {
+    if(this.currentUser) {
+      this.loggedin = true;
+      this.loginbutton = "Logout";
+    } else {
+      this.loggedin = false;
+      this.loginbutton = "Login";
+    }
+  }
+}
