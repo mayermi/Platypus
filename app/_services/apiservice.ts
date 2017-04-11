89d43782f5a0c511aaf31524d4cf44ecf038f5cb
Irina Anastasiu <irina.anastasiu@gmail.com>
2017-04-11 16:55:21 +1000

    Added another missing package.

--- app/_services/apiservice.ts
+++ app/_services/apiservice.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
-import 'rxjs/add/operator/map'
 import {User} from "../_models/user";
 
 @Injectable()
